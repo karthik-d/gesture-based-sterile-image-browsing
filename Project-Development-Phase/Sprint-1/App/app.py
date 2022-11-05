3ae90afdfaf344b40ff6c277362ced85f0910af4
anirudh <anirudh07.anand@gmail.com>
2022-11-05 10:10:11 +0530

    about page v1 added

--- Project-Development-Phase/Sprint-1/App/app.py
+++ Project-Development-Phase/Sprint-1/App/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/register")
 def register():
     return render_template('registration.html')
 
 @app.route("/login")
-def register():
+def login():
     return render_template('login.html')
 
+if __name__ == "__main__":
+    app.run(debug=True)
